ff5893765378e69e828e6e4acf17135b4afb9d45
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-04-02 09:49:03 -0300

    Added basic tests

--- brain/sqlgen.py
+++ brain/sqlgen.py
@@ -1,23 +1,67 @@
-from typing import List, Dict, Any
+from typing import List, Dict, Tuple, Any
 import dotenv
 import os
 import loguru
+import duckdb
 from sqlalchemy import create_engine
 
 dotenv.load_dotenv()
 logger = loguru.logger
 
 
-def get_db_url(conn: str = 'PGURL') -> str:
+def get_db_url(connvar: str = 'PGURL') -> str:
     """
     Returns the database connection url
     as defined in the environment variable `conn` 
-    :param conn: 
+    :param connvar: Environment variable containing the database connection URL
     :return: URL to database connection
     """
-    return os.getenv("DB_URL")
+    return os.getenv(connvar)
 
 
-def get_db() -> object:
-    engine = create_engine(get_db_url())
+def get_db(connvar='PGURL') -> object:
+    """
+    Returns the database connection object
+    :return:
+    """
+    engine = create_engine(get_db_url(connvar=connvar))
     return engine
+
+def get_duckdb_connection(url: str) -> object:
+    """
+    Returns a connection to a duckdb database
+    :param url: URL to the duckdb database
+    :return: duckdb connection object
+    """
+    if url == 'duckdb:///:memory:':
+        return duckdb.connect()
+    else: # for persistent databases
+        return duckdb.connect(url)
+
+
+def get_table_description(engine, table_name: str) -> List[Dict[str, Any]]:
+    """
+    Returns the description of the table using duckdb
+    :param engine:
+    :param table_name:
+    :return:
+    """
+    query = f"DESCRIBE SELECT * FROM {table_name};"
+    if isinstance(engine, duckdb.duckdb.DuckDBPyConnection):
+        result = engine.execute(query)
+    else:
+        with engine.connect() as conn:
+            result = conn.execute(query)
+
+    return result.fetchall()
+
+def get_csv_description(file_path: str) -> List[Tuple[str, Any]]:
+    """
+    Returns the description of the csv file using duckdb
+    :param file_path: file path or URL for remote file
+    :return: list of tuples
+    """
+    mdb = duckdb.connect()
+    query = f"DESCRIBE TABLE '{file_path}';"
+    result = mdb.execute(query).fetchall()
+    return result
---
+++ tests/brain/test_sqlgen.py
@@ -0,0 +1,45 @@
+import pytest
+from sqlalchemy import create_engine
+import os
+import duckdb
+from brain import sqlgen
+
+
+
+
+@pytest.fixture
+def memdb():
+    return duckdb.connect()
+
+tmp_path = pytest.fixture(lambda: './fixtures')
+
+def test_get_db_url():
+    os.environ['DBURL'] = 'duckdb:///:memory:'
+    assert sqlgen.get_db_url('DBURL') == 'duckdb:///:memory:'
+
+def test_get_db():
+    engine = sqlgen.get_db('PGURL')
+    assert isinstance(engine, object)
+
+def test_get_table_description():
+    table_name = 'startrek_table'
+    conn = sqlgen.get_duckdb_connection('duckdb:///:memory:')
+    query1 = f"CREATE TABLE {table_name} as SELECT * FROM read_csv('fixtures/Star_Trek-Season_1.csv');"
+
+    conn.execute(query1)
+    result = sqlgen.get_table_description(conn, table_name)
+
+    assert 'season_num' in result[0]
+    assert 'episode_num' in result[1]
+
+def test_get_csv_description_from_url():
+    file_path = 'https://blobs.duckdb.org/data/Star_Trek-Season_1.csv'
+    result = sqlgen.get_csv_description(file_path)
+    assert 'season_num' in result[0]
+    assert 'episode_num' in result[1]
+
+def test_get_csv_description_from_file():
+    file_path = 'fixtures/Star_Trek-Season_1.csv'
+    result = sqlgen.get_csv_description(file_path)
+    assert 'season_num' in result[0]
+    assert 'episode_num' in result[1]
