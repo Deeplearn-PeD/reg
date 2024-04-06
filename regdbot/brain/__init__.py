6f284fba4cc2d40f8bd1f751d96b8a0db07531da
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-04-06 19:53:28 -0300

    refactor: Update LLLModel with Ollama Client and modify response generation
    Updated LLLModel in the brain module to use Ollama Client instead of the previous method. Modified response generation in get_gemma_response method to better handle responses with Ollama Client.

--- regdbot/brain/__init__.py
+++ regdbot/brain/__init__.py
@@ -5,7 +5,7 @@ how it should interact with the user, and how it should respond to the user's re
 """
 from regdbot import Persona
 from openai import OpenAI
-import ollama
+from ollama import Client
 from regdbot.brain.sqlprompts import PromptTemplate
 import dotenv
 import os
@@ -15,14 +15,14 @@ dotenv.load_dotenv()
 
 class LLLModel:
     def __init__(self, model: str = 'gpt-4-0125-preview'):
-        self.llm = OpenAI(api_key=os.getenv('OPENAI_API_KEY')) if 'gpt' in model else ollama
+        self.llm = OpenAI(api_key=os.getenv('OPENAI_API_KEY')) if 'gpt' in model else Client(host='http://localhost:11434')
         self.model = model
 
     def get_response(self, question: str, context: str = None) -> str:
         if 'gpt' in self.model:
             return self.get_gpt_response(question, context)
         elif 'gemma' in self.model:
-            return self.get_gemma_response(question)
+            return self.get_gemma_response(question, context)
 
     def get_gpt_response(self, question: str, context: str)->str:
         response = self.llm.chat.completions.create(
@@ -44,15 +44,18 @@ class LLLModel:
         return response.choices[0].message.content
 
     def get_gemma_response(self, question: str, context: str) -> str:
-        response = self.llm.chat(
+        response = self.llm.generate(
             model=self.model,
-            messages=[
-                {'role': 'system', 'content': context},
-                {'role': 'user', 'content': question}
-            ],
-            stream=False
+            system=context,
+            prompt=question,
+            # messages=[
+            #     {'role': 'system', 'content': context},
+            #     {'role': 'user', 'content': question}
+            # ],
+            stream=True
         )
-        return response['message']['content']
+
+        return '/n'.join([resp['response'] for resp in response ])
 
 
 
