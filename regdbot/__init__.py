d4e81353e767ccacb318a14a1d9c660f91fb7ba6
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-04-06 10:40:35 -0300

    feat: Refactor PromptTemplate class for SQL query generation
    Updated the PromptTemplate class in the sqlprompts package to handle table descriptions and generate SQL query prompts more efficiently. The PromptTemplate class now provides a system preamble and allows for adding table descriptions, as well as generating prompts based on questions.

--- regdbot/__init__.py
+++ regdbot/__init__.py
@@ -1,14 +1,13 @@
 """
 This package defines a basic AI bot's Personality.
 Starting with a Base class setting the basic parameters
-for Such as name, language model uses, and basic context prompts defining its purpose.
+for Such as name, language model uses, and basic context sqlprompts defining its purpose.
 """
 from voice.talk import Speaker
+from regdbot.voice.persona_prompts import sql_retrieval_augmented
 from typing import List, Dict, Any, Union
 
-base_prompt = """
-You are Reggie D. Bot, a friendly AI assistant that helps users with data querying and analysis needs.
-"""
+
 
 class Persona:
     def __init__(self, name: str='Reggie D. Bot', languages=['pt_BR','en'], context_prompts: List[str] = None):
@@ -18,7 +17,7 @@ class Persona:
         self.model = None
         self.voice = Speaker(voice='faber-medium', language=self.active_language)
         self.say = self.voice.say
-        self.context_prompt = base_prompt + '' if context_prompts is None else '\n'.join(context_prompts)
+        self.context_prompt = sql_retrieval_augmented + '' if context_prompts is None else '\n'.join(context_prompts)
 
     def set_language(self, language: str):
         if language in self.languages:
--- regdbot/brain/sqlprompts/__init__.py
+++ regdbot/brain/sqlprompts/__init__.py
@@ -3,7 +3,7 @@ class PromptTemplate:
     Class to generate Prompt templates for interactions with the OpenAI API
     for SQL query generation.
     """
-    def __init__(self, prompt, dialect='postgresql'):
+    def __init__(self, dialect='postgresql'):
         self.system_preamble = f"""
         Given an input question about data in a relational database, 
         create a syntactically correct {dialect} query that will answer the question.
@@ -11,11 +11,21 @@ class PromptTemplate:
         You can use the following tables in your query:
         
         """
-        self.prompt = prompt
+        self._prompt = None
+
 
 
     def add_table_description(self, table_name, description):
         self.system_preamble += f"{table_name}: \n{description}"
-    def generate_code(self):
-        code = f"""
-        """
+
+
+    def get_prompt(self, question):
+        if self._prompt is None:
+            self._prompt = f"""
+            {self.system_preamble}
+            
+            Question: {question}
+            
+            SQL Query:
+            """
+        return self._prompt
