87b7f57e91b269c5066d53cab51d96a04735c331
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-04-30 15:28:48 -0300

    Added dependency on Base_agent and cleaned code

--- regdbot/__init__.py
+++ regdbot/__init__.py
@@ -3,20 +3,19 @@ This package defines a basic AI bot's Personality.
 Starting with a Base class setting the basic parameters
 for Such as name, language model uses, and basic context sqlprompts defining its purpose.
 """
-from regdbot.voice import talk
+from base_agent.voice import talk
 from regdbot.persona_prompts import sql_retrieval_augmented
+from base_agent import BasePersona
 from typing import List, Dict, Any, Union
 
 
 
-class Persona:
+class Persona(BasePersona):
     def __init__(self, name: str='Reggie D. Bot', model: str='gpt-4-0125-preview',  languages=['pt_BR','en'], ):
+        super().__init__(name=name, model=model, languages=languages)
         self.name = name
         self.languages = languages
         self.active_language = languages[0]
-        self.model = model
-        self.voice = talk.Speaker(language=self.active_language)
-        self.say = self.voice.say
         self.context_prompt = sql_retrieval_augmented[self.active_language]
 
     def set_language(self, language: str):
--- regdbot/brain/__init__.py
+++ regdbot/brain/__init__.py
@@ -7,6 +7,7 @@ from regdbot import Persona
 from openai import OpenAI
 from ollama import Client
 import ollama
+from base_agent.llminterface import LangModel
 from regdbot.brain.sqlprompts import PromptTemplate
 import dotenv
 import os
@@ -14,53 +15,20 @@ import os
 dotenv.load_dotenv()
 
 
-class LLLModel:
-    def __init__(self, model: str = 'gpt-4-0125-preview'):
-        self.llm = OpenAI(api_key=os.getenv('OPENAI_API_KEY')) if 'gpt' in model else Client(host='http://localhost:11434')
-        self.model = model
-
-    def get_response(self, question: str, context: str = None) -> str:
-        if 'gpt' in self.model:
-            return self.get_gpt_response(question, context)
-        elif 'gemma' in self.model:
-            return self.get_gemma_response(question, context)
-
-    def get_gpt_response(self, question: str, context: str)->str:
-        response = self.llm.chat.completions.create(
-            model=self.model,
-            messages=[
-                {
-                    'role': 'system',
-                    'content': context
-                },
-                {
-                    'role': 'user',
-                    'content': question
-                }
-            ],
-            max_tokens=100,
-            temperature=0.5,
-            top_p=1
-        )
-        return response.choices[0].message.content
-
-    def get_gemma_response(self, question: str, context: str) -> str:
-        response = ollama.generate(
-            model=self.model,
-            system=context[:2048],
-            prompt=question,
-        )
-
-        return response['response']
-        # return '/n'.join([resp['response'] for resp in response ])
-
-
 
 class RegDBot(Persona):
     def __init__(self, name: str = 'Reggie D. Bot', languages=['pt_BR', 'en'], model: str='gpt-4-0125-preview'):
         super().__init__(name=name, languages=languages,model=model)
-        self.llm = LLLModel(model=model)
+        self.llm = LangModel(model=model)
         self.prompt_template = None
+        self.context_prompt: str = ""
+
+    @property
+    def context(self):
+        return self.context_prompt
+
+    def set_context(self, context: str) -> None:
+        self.context_prompt = context
 
 
     def set_prompt(self, prompt_template):
