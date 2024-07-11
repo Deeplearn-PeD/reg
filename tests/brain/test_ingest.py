2b2175ef22b26fd1bfacce054cf68e2e4cbadd51
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-07-11 07:50:50 -0300

    formatting issues

--- tests/brain/test_ingest.py
+++ tests/brain/test_ingest.py
@@ -1,21 +1,22 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from pandas.testing import assert_frame_equal
+
 import pandas as pd
-from regdbot.brain.ingest import CSVIngestor
 from deltalake import DeltaTable
+from pandas.testing import assert_frame_equal
+
+from regdbot.brain.ingest import CSVIngestor
+
 
 class CSVIngestorTests(unittest.TestCase):
     test_file_path = 'brain/fixtures/journalcsv__doaj_20240710.csv.gz'
     test_output_path = '/tmp/doaj'
+
     def setUp(self):
         self.ingestor = CSVIngestor(self.test_file_path)
-
 
     def test_csv_file_is_correctly_ingested(self):
         # Given
         expected_df = pd.read_csv('brain/fixtures/journalcsv__doaj_20240710.csv.gz')
-
 
         # When
         result_df = self.ingestor.ingest()
@@ -23,15 +24,14 @@ class CSVIngestorTests(unittest.TestCase):
         # Then
         assert_frame_equal(result_df, expected_df)
 
-
     def test_data_is_written_to_delta_table(self):
         # When
         self.ingestor.ingest()
-        DL = self.ingestor.to_delta(self.test_output_path)
+        dl = self.ingestor.to_delta(self.test_output_path)
 
         # Then
 
-        self.assertIsInstance(DL, DeltaTable)
+        self.assertIsInstance(dl, DeltaTable)
 
     def test_ingestor_initializes_without_ingesting(self):
         # Given/When
@@ -40,7 +40,6 @@ class CSVIngestorTests(unittest.TestCase):
         # Then
         self.assertIsNone(ingestor.data)
 
-
     def test_raises_exception_when_csv_file_not_found(self):
         # Given
         ingestor = CSVIngestor('nonexistent.csv')
@@ -48,4 +47,3 @@ class CSVIngestorTests(unittest.TestCase):
         # When/Then
         with self.assertRaises(FileNotFoundError):
             ingestor.ingest()
-
