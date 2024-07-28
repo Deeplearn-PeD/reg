081f059bfaf56d5e50dc075ec65bc1fc1cf63a0d
Flávio Codeço Coelho <fccoelho@gmail.com>
2024-07-28 12:31:43 -0300

    Added memory module

---
+++ regdbot/brain/memory.py
@@ -0,0 +1,52 @@
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+import datetime
+
+class Problem(SQLModel, table=True):
+    id: int | None = Field(primary_key=True, default=None)
+    timestamp: datetime.datetime = Field(default_factory=datetime.datetime.now, index=True)
+    session_id: int = Field(primary_key=True, default=None)
+    question: str = Field()
+    context: str = Field()
+    code: str = Field()
+    explanation: str = Field()
+
+
+class History:
+    def __init__(self, dburl: str = "sqlite:///memory.db"):
+        self.engine = self.setup_db(dburl)
+
+    def setup_db(self, dburl: str):
+        """
+        Setup the database
+        :param dburl: database url
+        :return:
+        """
+        engine = create_engine(dburl)
+        SQLModel.metadata.create_all(engine)
+        return engine
+
+    def memorize(self, session_id: int, question: str, code: str, explanation: str, context: str):
+        """
+        Persist a chat in the database
+        :param session_id: id of the chat session
+        :param question: question asked
+        :param code: code snippet to answer the question
+        :param explanation: explanation of the code
+        :param context: context of the question
+        :return: memory object
+        """
+        with Session(self.engine) as session:
+            memory = Problem(session_id=session_id, question=question, code=code, explanation=explanation, context=context)
+            session.add(memory)
+            session.commit()
+            session.refresh(memory)
+            return memory
+
+    def recall(self, session_id, since: datetime.datetime = None):
+        with Session(self.engine) as session:
+            if since:
+                stmt = select(Problem).where(Problem.session_id == session_id).where(Problem.timestamp >= since)
+            else:
+                stmt = select(Problem).where(Problem.session_id == session_id)
+            results = session.exec(stmt)
+            return results
